fbbc3d97f20d8428d18d527d0a9f9be4d316aae5
Ahrndti <paul@fam-ahrndt.de>
2026-06-29 13:05:12 +0200

    Add PanelOptions interface and defaultOptions constant

---
+++ src/types.ts
@@ -0,0 +1,11 @@
+export interface PanelOptions {
+  fontSize: string;
+  showHeader: boolean;
+  wordWrap: boolean;
+}
+
+export const defaultOptions: PanelOptions = {
+  fontSize: '12px',
+  showHeader: true,
+  wordWrap: false,
+};
